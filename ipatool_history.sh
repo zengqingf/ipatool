496469a640076c81b3fe585ef6f0da6852652175
xcc <no-replay@gmail.com>
2025-07-14 03:58:21 +0800

    Update

--- ipatool_history.sh
+++ ipatool_history.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-./ipatool_history version-history --bundle-identifier com.xxx.xxx --max-versions 20
+read -p "请输入 Bundle Identifier: " BUNDLE_ID
+if [[ -z "$BUNDLE_ID" ]]; then
+  echo "错误：Bundle Identifier 不能为空！"
+  exit 1
+fi
+
+read -p "请输入最大版本数（默认 20）: " MAX_VERSIONS
+MAX_VERSIONS=${MAX_VERSIONS:-20}
+
+./ipatool_history version-history --bundle-identifier "$BUNDLE_ID" --max-versions "$MAX_VERSIONS"
--- ipatool_history_download.sh
+++ ipatool_history_download.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-./ipatool_history download --bundle-identifier com.xxx.xxx --version-id 12345
+read -p "请输入 Bundle Identifier: " BUNDLE_ID
+if [[ -z "$BUNDLE_ID" ]]; then
+  echo "错误：Bundle Identifier 不能为空！"
+  exit 1
+fi
+
+read -p "请输入 Version ID: " VERSION_ID
+if [[ -z "$VERSION_ID" ]]; then
+  echo "错误：Version ID 不能为空！"
+  exit 1
+fi
+
+./ipatool_history download --bundle-identifier "$BUNDLE_ID" --version-id "$VERSION_ID"
